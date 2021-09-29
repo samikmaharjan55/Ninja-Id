3201d423b254ebadd28ef28ea7b292a1b6d63903
Samik Maharjan <samikmaharjan55@gmail.com>
2021-09-29 12:44:21 +0545

    First branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,25 @@ class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text('Ninja ID'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text('Ninja ID'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            children: [
+              Text(
+                'NAME',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
